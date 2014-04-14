5d86c58556192283ca1ec4c367a320cc08fe3431
odawa <odawa@pixiv.co.jp>
2014-04-14 11:24:33 +0900

    sqliteを呼び出した

--- main.rb
+++ main.rb
@@ -1,7 +1,12 @@
 require 'sinatra'
+require 'sqlite3'
+
+db = SQLite3::Database.new "test.db"
+db.results_as_hash = true
 
 get '/' do
-  "Hello world"
+  posts = db.execute("SELECT * FROM posts ORDER BY id DESC")
+  erb :example, { :locals => { :posts => posts } }
 end
 
 get '/example' do
