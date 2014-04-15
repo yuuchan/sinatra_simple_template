8c256d4aa58ddfe1a22ad52cfa8d75bb77559c6d
catatsuy <catatsuy@catatsuy.org>
2014-04-15 12:51:36 +0900

    indent 直しました

--- main.rb
+++ main.rb
@@ -13,23 +13,23 @@ end
 post '/' do
   file_name = ""
   if params["file"]
-  	ext = ""
-  	if params["file"][:type].include? "jpeg"
-     ext = "jpg"
-   elsif params["file"][:type].include? "png"
-     ext = "png"
-   else
-     return "error"
-   end
-   file_name = "#{SecureRandom.hex}.#{ext}"
-   save_path = "./public/img/#{file_name}"
-   File.open(save_path,'wb') do |f|
-     f.write params["file"][:tempfile].read
-   end
- end
+    ext = ""
+    if params["file"][:type].include? "jpeg"
+      ext = "jpg"
+    elsif params["file"][:type].include? "png"
+      ext = "png"
+    else
+      return "error"
+    end
+    file_name = "#{SecureRandom.hex}.#{ext}"
+    save_path = "./public/img/#{file_name}"
+    File.open(save_path,'wb') do |f|
+      f.write params["file"][:tempfile].read
+    end
+  end
 
- stmt = db.prepare("INSERT INTO posts (username, title, body, filename, updated) VALUES (?, ?, ?, ?, datetime('now', '+09:00:00'))")
- stmt.bind_params(params["username"], params["title"], params["text"], file_name)
- stmt.execute
- redirect '/'
+  stmt = db.prepare("INSERT INTO posts (username, title, body, filename, updated) VALUES (?, ?, ?, ?, datetime('now', '+09:00:00'))")
+  stmt.bind_params(params["username"], params["title"], params["text"], file_name)
+  stmt.execute
+  redirect '/'
 end
