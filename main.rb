bf6ee4f0dc506fa09722bab95f84e837329d3839
odawarayu <odawa@pixiv.com>
2014-04-15 10:06:44 +0900

    画像を保存できるようになりました

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 require 'sqlite3'
+require 'pp'
+require 'securerandom'
 
 db = SQLite3::Database.new "test.db"
 db.results_as_hash = true
@@ -10,7 +12,22 @@ get '/' do
 end
 
 post '/' do
-  p params
+  pp params
+  if params["file"]
+  	ext = ""
+  	if params["file"][:type].include? "jpeg"
+  	  ext = "jpg"
+  	elsif params["file"][:type].include? "png"
+  	  ext = "png"
+  	else
+  	  return "error"
+    end
+    file_name = "#{SecureRandom.hex}.#{ext}"
+    save_path = "./public/img/#{file_name}"
+    File.open(save_path,'wb') do |f|
+  	  f.write params["file"][:tempfile].read
+    end
+  end
   stmt = db.prepare("INSERT INTO posts (title, body, updated) VALUES (?, ?, datetime('now', '+09:00:00'))")
   stmt.bind_params(params["title"], params["text"])
   stmt.execute
